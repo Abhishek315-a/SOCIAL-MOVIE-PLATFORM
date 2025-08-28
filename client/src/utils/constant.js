bcf4ae0b6d820d4c42f6803e0c0ffb29e9a772b2
Abhishek Kumar Singh <abhiits4@gmail.com>
2025-08-28 11:28:43 +0530

    Update constant.js

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT = "http://localhost:5000/user";
-export const TWEET_API_END_POINT = "http://localhost:5000/tweet";
-export const REACT_APP_API_KEY = "http://localhost:5000"
+export const USER_API_END_POINT = "https://social-movie-platform.onrender.com/user";
+export const TWEET_API_END_POINT = "https://social-movie-platform.onrender.com/tweet";
+export const REACT_APP_API_KEY = "https://social-movie-platform.onrender.com"
